8dd5bfadac2a199196e321cfc778f779492a64a7
zulus23 <zulus23@gmail.com>
2020-01-29 16:32:00 +0300

    create main page

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>First page</h1>
-    </div>
-  );
+    return (
+        <div className="gtk-main-container">
+
+            <div className='header'>
+                <h1>Поиск...</h1>
+            </div>
+            <div className='main'>
+            </div>
+            <div className='footer'>
+
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
